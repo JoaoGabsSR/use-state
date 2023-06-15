31ffb39530c250634c21a4ee3449ec29fd2200bf
João Gabriel <joaogabrielsr39@gmail.com>
2023-06-15 11:27:44 -0300

    ending project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [adress, setAdress] = useState({});
+
+  const adressManipulation = (event) => {
+    const cep = event.target.value ;
+
+    setAdress({ 
+      cep
+    });
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(res => res.json())
+        .then(data => {
+          setAdress(oldAdress => ({
+            ...oldAdress,
+            bairro: data.bairro,
+            cidade: data.localidade,
+            estado: data.uf
+          }));
+        });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" placeholder='Digite o CEP' onChange={adressManipulation}/>
+        <ul>
+          <li>CEP: {adress.cep}</li>
+          <li>Rua: {adress.rua}</li>
+          <li>Bairro: {adress.bairro}</li>
+          <li>Cidade: {adress.cidade}</li>
+          <li>Estado: {adress.estado}</li>
+        </ul>
       </header>
     </div>
   );
